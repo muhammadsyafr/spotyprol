eff2bc7492b40062194ebe10a14b13c6eccb655e
Syafrizal Muhammad <muhammadsyafr@gmail.com>
2024-12-15 22:39:50 +0700

    feat: update confg

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: "standalone",
-    basePath: "/spotyprol",
-    assetPrefix: "/spotyprol",
+    output: "export",
+    // basePath: "/spotyprol",
+    // assetPrefix: "/spotyprol",
 };
 
 export default nextConfig;
